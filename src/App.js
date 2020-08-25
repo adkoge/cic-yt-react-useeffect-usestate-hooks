260ee3c551c76a51867b652d97f43ee409c21a7f
Alan Koger <adkoge@gmail.com>
2020-08-25 08:47:01 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(12);
+
+  useEffect(() => {
+    console.log(`Count is: ${count}`);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Up and running</h1>
+      <button onClick={() => setCount(count + 1)}>Increment count</button>
     </div>
   );
 }
